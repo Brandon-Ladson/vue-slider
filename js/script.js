f9369147075c18118c8fa91ce4e48db8da8abc53
Brandon Ladson <brandonladson1999@gmail.com>
2020-11-17 13:54:46 +0100

    aggiunta file index.html e style.css e creazione file script.js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Ricreare lo slider di immagini ma questa volta con Vue.
+// Partiamo dallo zip che vi passo,
+// facciamo funzionare i bottoni next e prev
+// e mettiamo in pagina i pallini
+// BONUS:
+// - vediamo se troviamo una nostra tecnica per affrontare la cosa;
+// - facciamo si che sia evidenziato solo il pallino relativo all’img in corso;
+// - altro che volete provare.
